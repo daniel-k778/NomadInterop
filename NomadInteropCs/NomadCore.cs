92815bd026e702341da788d136d49b5f0625d272
daniel.k778 <danielkhoshkbarchi@gmail.com>
2024-07-29 09:09:54 -0600

    cs core

--- NomadInteropCs/NomadCore.cs
+++ NomadInteropCs/NomadCore.cs
@@ -5,7 +5,7 @@ namespace NomadLibrary
 {
     public class NomadCore
     {
-        const string dllpath = "C:\\Users\\Daniel\\OneDrive\\Desktop\\Nomad\\NomadInterop\\x64\\Debug\\NomadInteropCpp.dll";
+        private const string dllpath = "C:\\Users\\Daniel\\OneDrive\\Desktop\\Nomad\\NomadInterop\\x64\\Debug\\NomadInteropCpp.dll";
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void EvaluateDelegate(IntPtr x, int m_NumVars, int numConstraints);
 
@@ -77,70 +77,5 @@ namespace NomadLibrary
 
             return results;
         }
-
-
-
-        //static void Main()
-        //{
-        //    IntPtr nomadCore = CreateNomadCore();
-        //    //SetOutputPath(nomadCore, "sol.txt");
-        //    SetNumberVariables(nomadCore, 5);
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        SetInitialVariable(nomadCore, i, 1.0);
-        //        SetUpperBound(nomadCore, i, 5.0);
-        //        SetLowerBound(nomadCore, i, -6.0);
-        //        SetVariableType(nomadCore, i, "CONTINUOUS");
-        //    }
-
-        //    SetNumberOfIterations(nomadCore, 100);
-        //    SetNumberEBConstraints(nomadCore, 2);
-        //    SetNumberPBConstraints(nomadCore, 3);
-
-        //    myEvaluator userEvaluator = new myEvaluator();
-
-        //    SetCustomEvaluator(nomadCore, userEvaluator);
-
-        //    Optimize(nomadCore);
-        //}
     }
-
-    //public class myEvaluator : IUserEvaluator
-    //{
-    //    private double obj;
-    //    private double[] constraints;
-
-    //    public myEvaluator()
-    //    {
-    //        obj = 0.0;
-    //        constraints = new double[2]; // Assuming 2 constraints for simplicity
-    //    }
-
-    //    public void Evaluate(IntPtr x, int m_NumVars, int numConstraints)
-    //    {
-    //        double[] xArray = new double[m_NumVars];
-    //        Marshal.Copy(x, xArray, 0, m_NumVars);
-
-    //        double c1 = 0.0, c2 = 0.0;
-    //        for (int i = 0; i < m_NumVars; i++)
-    //        {
-    //            c1 += Math.Pow((xArray[i] - 1.0), 2);
-    //            c2 += Math.Pow((xArray[i] + 1), 2);
-    //        }
-
-    //        obj = xArray[4];
-    //        constraints[0] = c1 - 25;
-    //        constraints[1] = 25 - c2;
-    //    }
-
-    //    public double GetObjectiveFunction()
-    //    {
-    //        return obj;
-    //    }
-
-    //    public void GetConstraints(IntPtr constraintsPtr)
-    //    {
-    //        Marshal.Copy(constraints, 0, constraintsPtr, constraints.Length);
-    //    }
-    //}
 }
